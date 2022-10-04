acdae90ee8f64d1a47b604471ee2d0c4cda587c0
Olgrin <matrica_1997@mail.ru>
2022-10-04 12:54:36 +0300

    execution of the first point of the algorithm

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int n, m=0, i;
+Console.Write("Введите количество строк в массиве: ");
+n = Int16.Parse(Console.ReadLine());
+string[] array = new string[n];
+for(i=0;i<n;i++)
+{
+    Console.Write($"Введите {i}-ю строку массива: ");
+    array[i] = Console.ReadLine();
+}
+
